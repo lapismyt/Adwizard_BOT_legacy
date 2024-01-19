27c3f48393a4451dbf09d506092093259fdbb520
LapisMYT <nikitagavrilin005@gmail.com>
2024-01-19 17:58:38 +0300

    .

--- models.py
+++ models.py
@@ -25,8 +25,8 @@ class Data(Struct):
             if usr.id == id:
                 if usr.settings.model == "gpt-4-0613":
                     usr.settings.model = "gpt-4"
-                if usr.settings.model == "gpt-3.5-turbo":
-                    usr.settings.model == "gpt-3.5"
+                if usr.settings.model == "gpt-3.5":
+                    usr.settings.model == "gpt-3.5-turbo"
                 return usr
         return None
 
