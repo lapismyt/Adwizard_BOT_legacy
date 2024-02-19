153554843e128952e576d95519d7897fc8ec7227
LapisMYT <nikitagavrilin005@gmail.com>
2024-02-19 19:20:04 +0300

    .

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ GPT_MODELS = [
     "gpt-3.5-turbo-instruct-0914",
     "gpt-3.5-turbo-16k-0613",
     "gpt-4",
-    "gpt-4-0613"
+    "gpt-4-0613",
     "gpt-4-1106-preview",
     "gpt-4-turbo-preview",
     "gpt-4-0125-preview",
