2d95576c7fc1c3153ff244e16f169b7558e435e0
LapisMYT <nikitagavrilin005@gmail.com>
2024-02-20 19:14:22 +0300

    .

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ GPT_MODELS = [
     "gpt-3.5-turbo",
     "gpt-3.5-turbo-1106",
     "gpt-3.5-turbo-0125",
-    "gpt-3.5-turbo-0301"
+    "gpt-3.5-turbo-0301",
     "gpt-3.5-turbo-16k",
     "gpt-3.5-turbo-16k-0613",
     "gpt-3.5-turbo-instruct",
