dd8cf372422eecd6d8bb84020ab00a48e6628884
LapisMYT <nikitagavrilin005@gmail.com>
2024-02-20 22:45:58 +0300

    .

--- bot.py
+++ bot.py
@@ -257,7 +257,7 @@ def handle_req(message, text, skipped=False):
                 messages = conv,
                 temperature = 0.8,
                 stream = False,
-                max_tokens = 3072
+                max_tokens = 1024
             )
             response = response.choices[0].message.content
             data = models.Data.load()
